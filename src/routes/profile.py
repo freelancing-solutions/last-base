236de30e5865134c20aac0a8965dae0fdd70ec05
freelancing-solutions <mobiusndou@gmail.com>
2024-03-04 15:21:14 +0200

    added settings screen

--- src/routes/profile.py
+++ src/routes/profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, send_from_directory
 from src.utils import static_folder
 from src.main import user_controller
+
 profile_route = Blueprint('profile', __name__)
 
 
@@ -11,3 +12,12 @@ async def get_profile():
     data = await user_controller.get_profile_by_game_id(game_id='3XZXLABF')
     context = dict(profile=data)
     return render_template('profile.html', **context)
+
+
+@profile_route.get('/dashboard/settings')
+async def get_settings():
+    context = {}
+
+    data = await user_controller.get_profile_by_game_id(game_id='3XZXLABF')
+    context = dict(profile=data)
+    return render_template('config.html', **context)
