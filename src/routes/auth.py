52db9ff5b89103a428a16c00b0e0823a975539e2
freelancing-solutions <mobiusndou@gmail.com>
2024-03-04 11:14:34 +0200

    login and password reset

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, send_from_directory
+from flask import Blueprint, render_template, send_from_directory, flash
 from src.utils import static_folder
 
 auth_route = Blueprint('auth', __name__)
@@ -10,5 +10,17 @@ def get_auth():
     return render_template('login.html', **context)
 
 
+@auth_route.get('/password-reset')
+def get_password_reset():
+    context = {}
+    return render_template('password_reset.html', **context)
 
 
+@auth_route.post('/password-reset')
+def do_password_reset():
+    context = {}
+    # Check if User Email is available
+    # Send Message to User with a link to reset password
+    # Flash the message that email was sent with proper details never indicate failure
+    flash(message="Message with a link to reset your password has been sent", category='success')
+    return render_template('index.html', **context)
