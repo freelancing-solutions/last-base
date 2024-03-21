d7635ceecbcdd8b2785728557a80e40be9445d09
freelancing-solutions <mobiusndou@gmail.com>
2024-03-22 00:16:14 +0200

    formalized merchant account approval

--- src/main/bootstrapping.py
+++ src/main/bootstrapping.py
@@ -4,9 +4,10 @@ def bootstrapper():
     from src.database.sql.game import GameIDSORM, GiftCodesORM, RedeemCodesORM
     from src.database.sql.wallet import WalletTransactionORM, WalletORM
     from src.database.sql.market import BuyerAccountORM, SellerAccountORM
+    from src.database.sql.email_service import EmailServiceORM
 
     classes_to_create = [UserORM, PayPalORM, ProfileORM, NotificationORM, GameIDSORM, GiftCodesORM, RedeemCodesORM,
-                         WalletTransactionORM, WalletORM, BuyerAccountORM, SellerAccountORM]
+                         WalletTransactionORM, WalletORM, BuyerAccountORM, SellerAccountORM, EmailServiceORM]
 
     for cls in classes_to_create:
         cls.create_if_not_table()
