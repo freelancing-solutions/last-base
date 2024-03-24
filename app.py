833bd75e7590a30919110c5cc3df2364f1ba87af
freelancing-solutions <mobiusndou@gmail.com>
2024-03-24 18:37:15 +0200

    Email Service Dashboard completed

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ from src.config import config_instance
 app, chat_io = create_app(config=config_instance())
 
 if __name__ == '__main__':
-    chat_io.run(app, host='0.0.0.0', port=5000, debug=True)
+    chat_io.run(app, port=5000, debug=True)
     # app.run(debug=True)
