8fe800cc3e8a4c7ed2b4d6bbb2ae1f72026bb829
freelancing-solutions <mobiusndou@gmail.com>
2024-03-26 16:06:08 +0200

    Avoided User Conflicts in Chat

--- src/controller/chat_controller.py
+++ src/controller/chat_controller.py
@@ -1,6 +1,6 @@
 import random
 
-from src.controller import Controllers
+from src.controller import Controllers, error_handler
 from src.database.models.support_chat import ChatMessage, ChatUser
 from src.database.sql.support_chat import ChatMessageORM, ChatUserORM
 
@@ -103,41 +103,45 @@ class ChatController(Controllers):
         :param message:
         :return:
         """
-        with self.get_session() as session:
-            chat_message_orm = session.query(ChatMessageORM).filter(
-                ChatMessageORM.message_id == message.message_id).first()
-            # TODO please consider saving a list of users in this class
+        try:
+            with self.get_session() as session:
+                chat_message_orm = session.query(ChatMessageORM).filter(
+                    ChatMessageORM.message_id == message.message_id).first()
+                # TODO please consider saving a list of users in this class
 
-            if isinstance(chat_message_orm, ChatMessageORM):
+                if isinstance(chat_message_orm, ChatMessageORM):
+                    return message
+                chat_message_orm = ChatMessageORM(uid=message.uid,
+                                                  message_id=message.message_id,
+                                                  text=message.text,
+                                                  timestamp=message.timestamp)
+                session.add(chat_message_orm)
+                session.commit()
+                used_hexes = list(self.user_colour.keys())
+                if message.uid not in used_hexes:
+                    self.user_colour[message.uid] = create_colour(used_hexes=used_hexes)
+
+                message.user_colour = self.user_colour[message.uid]
+
                 return message
-            chat_message_orm = ChatMessageORM(uid=message.uid,
-                                              message_id=message.message_id,
-                                              text=message.text,
-                                              timestamp=message.timestamp)
-            session.add(chat_message_orm)
-            session.commit()
-            used_hexes = list(self.user_colour.keys())
-            if message.uid not in used_hexes:
-                self.user_colour[message.uid] = create_colour(used_hexes=used_hexes)
-
-            message.user_colour = self.user_colour[message.uid]
-
+        except Exception as e:
+            print(f"An Error Occured  {str(e)}")
             return message
 
+    @error_handler
     async def get_all_messages(self) -> list[ChatMessage]:
         """
             Get All Messages from database
         :return:
         """
         with self.get_session() as session:
-            print("Entering Get ALL Messages")
             chat_messages_orm_list = session.query(ChatMessageORM).all()
             chat_users_orm_list = session.query(ChatUserORM).all()
 
             chat_users = [ChatUser(**user.to_dict()) for user in chat_users_orm_list]
             chat_messages = [ChatMessage(**message.to_dict()) for message in chat_messages_orm_list if
                              isinstance(message, ChatMessageORM)]
-            # print(f"Found Original Chat Messages : {len(chat_messages)}")
+
             proc_messages = []
             for message in chat_messages:
                 used_hexes = list(self.user_colour.keys())
@@ -147,15 +151,9 @@ class ChatController(Controllers):
                 message.user_colour = self.user_colour[message.uid]
                 proc_messages.append(message)
 
-                # for user in chat_users:
-                #     if user.uid == message.uid:
-                #         print(user.colour)
-                #         message.user_colour = user.colour
-
-            # print(f"Proc Messages : {len(proc_messages)}")
-
             return proc_messages
 
+    @error_handler
     async def get_all_users(self) -> list[str]:
         colours_reversed = {value: key for key, value in colours.items()}
         return [colours_reversed[_hex] for _hex in self.user_colour.values()]
