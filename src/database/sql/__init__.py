b57a7098717fce752d94a215838756738c38ad57
freelancing-solutions <mobiusndou@gmail.com>
2024-04-01 21:22:45 +0200

    created a listing editor

--- src/database/sql/__init__.py
+++ src/database/sql/__init__.py
@@ -6,7 +6,8 @@ from src.config import config_instance
 
 settings = config_instance().MYSQL_SETTINGS
 # Replace 'your_username', 'your_password', 'your_host', and 'your_database' with your MySQL database credentials
-engine = create_engine(settings.DEVELOPMENT_DB)
+timeout_seconds = 60
+engine = create_engine(settings.DEVELOPMENT_DB, connect_args={'connect_timeout': timeout_seconds})
 Session = sessionmaker(bind=engine)
 session = Session()
 
